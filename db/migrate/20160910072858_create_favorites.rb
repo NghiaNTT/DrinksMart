67dc2263e98e0006e6dc699ab7401c13423c0866
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-09-10 15:39:36 +0700

    delete table favorite

--- db/migrate/20160910072858_create_favorites.rb
+++
@@ -1,9 +0,0 @@
-class CreateFavorites < ActiveRecord::Migration[5.0]
-  def change
-    create_table :favorites do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :drink, index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end
